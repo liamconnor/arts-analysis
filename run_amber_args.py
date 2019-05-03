7c97916eee5e09fe24d2af1d114d71c76e8707b0
Liam Connor <liam.dean.connor@gmail.com>
2019-05-03 14:57:35 +0200

    more string manipulation

--- run_amber_args.py
+++ run_amber_args.py
@@ -3,7 +3,7 @@ import sys
 
 import glob
 
-def execute_amber(file, nbatch=10800, hdr=460, 
+def execute_amber(fn, nbatch=10800, hdr=460, 
 				  rfi_option="-rfim", snr="mom_sigmacut", snrmin=6,
 				  nchan=1536, pagesize=12500, chan_width=0.1953125, 
 				  min_freq=1249.700927734375, tsamp=8.192e-05, output_prefix="./"):
@@ -12,14 +12,21 @@ def execute_amber(file, nbatch=10800, hdr=460,
 
 	if snr == "momad":
 	    conf_dir = "/home/oostrum/tuning/tuning_survey/momad/amber_conf"
-	    snr="-snr_momad -max_file $conf_dir/max.conf -mom_stepone_file $conf_dir/mom_stepone.conf -mom_steptwo_file $conf_dir/mom_steptwo.conf -momad_file $conf_dir/momad.conf"
+
+	    str_args_snr = (conf_dir, conf_dir, conf_dir, conf_dir)
+	    snr="-snr_momad -max_file %s/max.conf \
+	    	-mom_stepone_file %s/mom_stepone.conf -mom_steptwo_file \
+	    	%s/mom_steptwo.conf -momad_file %s/momad.conf" % str_args_snr
 	elif snr == "mom_sigmacut":
 	    conf_dir = "/home/oostrum/tuning/tuning_survey/mom_sigmacut/amber_conf"
-	    snr="-snr_mom_sigmacut -max_std_file $conf_dir/max_std.conf -mom_stepone_file $conf_dir/mom_stepone.conf -mom_steptwo_file $conf_dir/mom_steptwo.conf"
-	else:
-	    print("Unknown SNR mode: $snr")
 
-	str_args_general = (conf_dir, conf_dir, conf_dir, conf_dir, conf_dir, snrmin, conf_dir, conf_dir)
+	    str_args_snr = (conf_dir, conf_dir, conf_dir)
+	    snr="-snr_mom_sigmacut -max_std_file %s/max_std.conf -mom_stepone_file \
+	    	%s/mom_stepone.conf -mom_steptwo_file %s/mom_steptwo.conf" % str_args_snr
+	else:
+	    print("Unknown SNR mode: %s" % snr)
+
+	str_args_general = (conf_dir, conf_dir, conf_dir, conf_dir, conf_dir, snrmin, conf_dir, conf_dir, conf_dir)
 	general="amber -opencl_platform 0 -sync -print -padding_file \
 			 %s/padding.conf -zapped_channels %s/zapped_channels_1400.conf \
 			 -integration_file %s/integration.conf -subband_dedispersion \
@@ -27,17 +34,17 @@ def execute_amber(file, nbatch=10800, hdr=460,
 			 -dedispersion_steptwo_file %s/dedispersion_steptwo.conf \
 			 -threshold %d -time_domain_sigma_cut -time_domain_sigma_cut_steps \
 			 %s/tdsc_steps.conf -time_domain_sigma_cut_configuration %s/tdsc.conf \
-			 -downsampling_configuration $conf_dir/downsampling.conf" % str_args_general
+			 -downsampling_configuration %s/downsampling.conf" % str_args_general
 
-	str_args_fil = (hdr, nbatch, chan_width, min_freq, nchan, pagesize, tsamp)
-	fil="-sigproc -stream -header %d -data $file -batches %d \
+	str_args_fil = (hdr, fn, nbatch, chan_width, min_freq, nchan, pagesize, tsamp)
+	fil="-sigproc -stream -header %d -data %s -batches %d \
 		-channel_bandwidth %f -min_freq %f -channels %d \
 		-samples %d -sampling_time %f" % str_args_fil
 
-	str_args_step1 = (general, rfi_option, snr, fil, output_prefix)
+	str_args_step1 = (general, rfi_option, snr, fn, conf_dir, output_prefix)
 	amber_step1="%s %s %s %s -opencl_device 1 \
 				 -device_name ARTS_step1_81.92us_1400MHz \
-				 -integration_steps $conf_dir/integration_steps_x1.conf \
+				 -integration_steps %s/integration_steps_x1.conf \
 				 -subbands 32 -dms 32 -dm_first 0 -dm_step 0.2 -subbanding_dms 64 \
 				 -subbanding_dm_first 0 -subbanding_dm_step 6.4 \
 				 -output %s_step1" % str_args_step1
