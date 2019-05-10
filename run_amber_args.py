be4bdc16f61d4a4ec4eae6702e0fc9546db59f0f
Liam Connor <liam.dean.connor@gmail.com>
2019-05-10 09:59:36 +0200

    mock pipeline script runs everything from amber to classifier

--- run_amber_args.py
+++ run_amber_args.py
@@ -15,9 +15,7 @@ def execute_amber(fn, nbatch=10800, hdr=460,
         conf_dir = "/home/oostrum/tuning/tuning_survey/momad/amber_conf"
 
         str_args_snr = (conf_dir, conf_dir, conf_dir, conf_dir)
-        snr="-snr_momad -max_file %s/max.conf \
-            -mom_stepone_file %s/mom_stepone.conf -mom_steptwo_file \
-            %s/mom_steptwo.conf -momad_file %s/momad.conf" % str_args_snr
+        snr="-snr_momad -max_file %s/max.conf -mom_stepone_file %s/mom_stepone.conf -mom_steptwo_file %s/mom_steptwo.conf -momad_file %s/momad.conf" % str_args_snr
     elif snr == "mom_sigmacut":
         conf_dir = "/home/oostrum/tuning/tuning_survey/mom_sigmacut/amber_conf"
 #        conf_dir = "/home/arts/ARTS-obs/amber_conf/"
@@ -27,30 +25,19 @@ def execute_amber(fn, nbatch=10800, hdr=460,
         print("Unknown SNR mode: %s" % snr)
 
     str_args_general = (conf_dir, conf_dir, conf_dir, conf_dir, conf_dir, snrmin, conf_dir, conf_dir, conf_dir)
-    general="amber -opencl_platform 0 -sync -print -padding_file %s/padding.conf -zapped_channels %s/zapped_channels_1400.conf \
-    -integration_file %s/integration.conf -subband_dedispersion -dedispersion_stepone_file %s/dedispersion_stepone.conf \
-    -dedispersion_steptwo_file %s/dedispersion_steptwo.conf -threshold %d -time_domain_sigma_cut \
-    -time_domain_sigma_cut_steps %s/tdsc_steps.conf -time_domain_sigma_cut_configuration %s/tdsc.conf \
-    -downsampling_configuration %s/downsampling.conf -compact_results" % str_args_general
+    general="amber -opencl_platform 0 -sync -print -padding_file %s/padding.conf -zapped_channels %s/zapped_channels_1400.conf -integration_file %s/integration.conf -subband_dedispersion -dedispersion_stepone_file %s/dedispersion_stepone.conf -dedispersion_steptwo_file %s/dedispersion_steptwo.conf -threshold %d -time_domain_sigma_cut -time_domain_sigma_cut_steps %s/tdsc_steps.conf -time_domain_sigma_cut_configuration %s/tdsc.conf -downsampling_configuration %s/downsampling.conf -compact_results" % str_args_general
 
     str_args_fil = (hdr, fn, nbatch, chan_width, min_freq, nchan, pagesize, tsamp)
-    fil="-sigproc -stream -header %d -data %s -batches %d \
-    -channel_bandwidth %f -min_freq %f -channels %d \
-    -samples %d -sampling_time %0.5e" % str_args_fil
+    fil="-sigproc -stream -header %d -data %s -batches %d -channel_bandwidth %f -min_freq %f -channels %d -samples %d -sampling_time %0.5e" % str_args_fil
 
     str_args_step1 = (general, rfi_option, snr, fil, conf_dir, output_prefix)
-    amber_step1="%s %s %s %s -opencl_device 1 -device_name ARTS_step1_81.92us_1400MHz -integration_steps %s/integration_steps_x1.conf \
-    -subbands 32 -dms 32 -dm_first 0 -dm_step 0.2 -subbanding_dms 64 -subbanding_dm_first 0 -subbanding_dm_step 6.4 -output %s_step1" % str_args_step1
+    amber_step1="%s %s %s %s -opencl_device 1 -device_name ARTS_step1_81.92us_1400MHz -integration_steps %s/integration_steps_x1.conf -subbands 32 -dms 32 -dm_first 0 -dm_step 0.2 -subbanding_dms 64 -subbanding_dm_first 0 -subbanding_dm_step 6.4 -output %s_step1" % str_args_step1
 
     str_args_step2 = (general, rfi_option, snr, fil, conf_dir, output_prefix)
-    amber_step2="%s %s %s %s -opencl_device 2 -device_name ARTS_step2_81.92us_1400MHz \
-    -integration_steps %s/integration_steps_x1.conf -subbands 32 -dms 32 -dm_first 0 -dm_step 0.2 -subbanding_dms 64 \
-    -subbanding_dm_first 409.6 -subbanding_dm_step 6.4 -output %s_step2" % str_args_step2
+    amber_step2="%s %s %s %s -opencl_device 2 -device_name ARTS_step2_81.92us_1400MHz -integration_steps %s/integration_steps_x1.conf -subbands 32 -dms 32 -dm_first 0 -dm_step 0.2 -subbanding_dms 64 -subbanding_dm_first 409.6 -subbanding_dm_step 6.4 -output %s_step2" % str_args_step2
 
     str_args_step3 = (general, rfi_option, snr, fil, conf_dir, output_prefix)
-    amber_step3="%s %s %s %s -opencl_device 3 -device_name ARTS_step3_nodownsamp_81.92us_1400MHz \
-    -integration_steps %s/integration_steps_x1.conf -subbands 32 -dms 16 -dm_first 0 -dm_step 2.5 \
-    -subbanding_dms 64 -subbanding_dm_first 819.2 -subbanding_dm_step 40.0 -output %s_step3" % str_args_step3
+    amber_step3="%s %s %s %s -opencl_device 3 -device_name ARTS_step3_nodownsamp_81.92us_1400MHz -integration_steps %s/integration_steps_x1.conf -subbands 32 -dms 16 -dm_first 0 -dm_step 2.5 -subbanding_dms 64 -subbanding_dm_first 819.2 -subbanding_dm_step 40.0 -output %s_step3" % str_args_step3
 
     thread_step1 = threading.Thread(target=os.system, args=[amber_step1])
     thread_step2 = threading.Thread(target=os.system, args=[amber_step2])
