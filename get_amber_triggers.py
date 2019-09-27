6a1a39edeac85c7f05333451c1de1d16987abe36
ARTS <arts@arts041.apertif>
2019-09-27 12:45:18 +0000

    script retrieves amber triggers from all nodes

--- get_amber_triggers.py
+++ get_amber_triggers.py
@@ -11,6 +11,10 @@ import numpy as np
 
 import tools
 
+if len(sys.argv) < 2:
+    print("Expecting, e.g. python get_amber_triggers.py 20190925/2019-09-25-22:50:52.FRB190709 /data1/data/FRBs/FRB190925/amber/ 956.0")
+    exit()
+
 directory = sys.argv[1]
 outdir = sys.argv[2]
 
@@ -23,9 +27,20 @@ for ii in range(40):
     os.system('scp arts0%0.2d:/data2/output/%s/amber/CB%0.2d.trigger %s' % (ii+1,directory,ii,outdir))
     if dm0>0:
         fn = outdir + 'CB%0.2d.trigger' % ii
+
+        if not os.path.isfile(fn):
+            continue
+
         dm, sig, tt, downsample, beam = tools.read_singlepulse(fn, beam='all')
+
+        if len(dm)==0:
+            continue 
+
         ind = np.where(np.abs(dm-dm0)<10)[0]
-        for jj in ind:
+
+        if len(ind)>0:
             print(fn)
+
+        for jj in ind:
             print("DM:%0.2f T:%0.2f S/N:%0.2f" % (dm[jj], tt[jj], sig[jj]))
         print("")
