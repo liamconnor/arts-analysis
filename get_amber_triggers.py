2d88b6b6b938618e9a98f278f966fca44e15818e
Liam Connor <liam.dean.connor@gmail.com>
2019-11-19 18:27:53 +0100

    fixing parser

--- get_amber_triggers.py
+++ get_amber_triggers.py
@@ -37,33 +37,84 @@ print(directory)
 print(outdir)
 print(dm0)
 
-for ii in CBs:
-    os.system('scp arts0%0.2d:/data2/output/%s/amber/CB%0.2d.trigger %s' % (ii+1,directory,ii,outdir))
-    print('scp arts0%0.2d:/data2/output/%s/amber/CB%0.2d.trigger %s' % (ii+1,directory,ii,outdir))
-    if dm0>0:
-        fn = outdir + 'CB%0.2d.trigger' % ii
+if __name__=='__main__':
+    def foo_callback(option, opt, value, parser):
+      setattr(parser.values, option.dest, value.split(','))
 
-        if not os.path.isfile(fn):
-            continue
+    parser = optparse.OptionParser(prog="snr_per_beam.py", \
+                        version="", \
+                        usage="%prog fndir [OPTIONS]", \
+                        description="Get SNR for each beam (CB or SB)")
+    parser.add_option('--sig_thresh', dest='sig_thresh', type='float', \
+                        help="Only process events above >sig_thresh S/N" \
+                                "(Default: 5.0)", default=5.0)
+    parser.add_option('--t0', dest='t0', type='float', \
+                        help="Arrival time of pulse", default=0.0)
+    parser.add_option('--save_data', dest='save_data', action='store_true', \
+                        help="save data", default=False)
+    parser.add_option('--mk_plot', dest='mk_plot', action='store_true', \
+                        help="make plot if True (default False)", default=False)
+    parser.add_option('--dm_min', dest='dm_min', type='float',
+                        help="", 
+                        default=0.0)
+    parser.add_option('--dm_max', dest='dm_max', type='float',
+                        help="", 
+                        default=np.inf)
+    parser.add_option('--t_max', dest='t_max', type='float',
+                        help="Only process first t_max seconds", 
+                        default=np.inf)
+    parser.add_option('--t_window', dest='t_window', type='float',
+                        help="", 
+                        default=0.1)
+    parser.add_option('--sb_ref', dest='sb_ref', type=int,
+                        help="", 
+                        default=None)
+    parser.add_option('--cb_ref', dest='cb_ref', type=int,
+                        help="", 
+                        default=0)
+    parser.add_option('--CBs',
+                  type='string',
+                  action='callback',
+                  callback=foo_callback, 
+                  )
 
-        dm, sig, tt, downsample, beam = tools.read_singlepulse(fn, beam='all')
+    print(options.CBs)
 
-        if len(dm)==0:
-            continue 
+    parser = OptionParser()
 
-        if t0_==0:
-            t0 = tt
-        else:
-            t0 = t0_
 
-#        ind = np.where((np.abs(dm-dm0)<5) & (np.abs(tt-t0)<5.0))[0]
-        ind = np.where((np.abs(dm-dm0)<15.))[0]
-        
-        if len(ind)>0:
-            print(fn)
-        else:
-            print("\nNo corresponding triggers\n")
+    dm_max = options.dm_max
+    dm_min = options.dm_min 
+    sig_thresh = options.sig_thresh
+    CBs = range(40)
 
-        for jj in ind:
-            print("DM:%0.2f SB:%d T:%0.2f S/N:%0.2f" % (dm[jj], beam[jj], tt[jj], sig[jj]))
-        print("")
+    for ii in CBs:
+        os.system('scp arts0%0.2d:/data2/output/%s/amber/CB%0.2d.trigger %s' % (ii+1,directory,ii,outdir))
+        print('scp arts0%0.2d:/data2/output/%s/amber/CB%0.2d.trigger %s' % (ii+1,directory,ii,outdir))
+        if dm0>0:
+            fn = outdir + 'CB%0.2d.trigger' % ii
+
+            if not os.path.isfile(fn):
+                continue
+
+            dm, sig, tt, downsample, beam = tools.read_singlepulse(fn, beam='all')
+
+            if len(dm)==0:
+                continue 
+
+            if t0_==0:
+                t0 = tt
+            else:
+                t0 = t0_
+
+            ind = np.where((np.abs(dm-dm0)<5) & () (np.abs(tt-t0)<5.0))[0]
+            ind = np.where((dm<dm_max) & (dm>dm_min) & (np.abs(tt-t0)<5.0) & (sig>sig_thresh))[0]    
+            
+            if len(ind)>0:
+                print(fn)
+            else:
+                print("\nNo corresponding triggers\n")
+
+            for jj in ind:
+                print("DM:%0.2f SB:%d T:%0.2f S/N:%0.2f" % (dm[jj], beam[jj], tt[jj], sig[jj]))
+            print("")
