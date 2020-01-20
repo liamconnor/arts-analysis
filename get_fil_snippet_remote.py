7344684702e0ca6ed720e7f3505a262f4fb520aa
ARTS <arts@arts041.apertif>
2020-01-20 17:59:29 +0000

    All data now collected in arts041

--- get_fil_snippet_remote.py
+++ get_fil_snippet_remote.py
@@ -1,5 +1,5 @@
 import os
-
+import socket
 import optparse
 
 fnscript='/home/arts/connor/software/arts-analysis/arts-analysis/get_fil_snippet.py'
@@ -13,8 +13,12 @@ if __name__=='__main__':
 
 
     parser.add_option('--outdir', dest='outdir', type='str',
-                      help="directory to write data to",
+                      help="directory to write data to on the node",
                       default='./data/')
+
+    parser.add_option('--collectdir', dest='collectdir', type='str',
+                      help="directory on arts041 to copy the data to",
+                      default='')
 
     parser.add_option('--T', dest='T', type='float',
                       help="time of data snippet in seconds",
@@ -37,8 +41,28 @@ if __name__=='__main__':
     options, args = parser.parse_args()
     fn_fil = args[0]
 
+    forked_jobs = []
+    os.system('on_all_nodes.sh "mkdir -p %s"' % options.outdir) # in case it does not exist yet
     for ii in range(ncb):
-        ansible_args = (ii+1, fnscript, fn_fil, ii, options.t, 
-                        options.T, options.beams, options.central_freq,
-                        options.outdir)
-        os.system('ansible "arts0%0.2d.apertif" -a "python %s %s/CB%0.2d --t %f --T %f --beams %s --central_freq %f --outdir %s"' % ansible_args)
+#        print("Forking node " + str(ii+1))
+#        os.system('sleep 5')
+        child = os.fork()
+        if child: # have the child, i.e. be the parent
+            forked_jobs.append(child)
+        else:
+            # create the TAB snippet(s)
+            ansible_args = (ii+1, fnscript, fn_fil, ii, ii, options.t, 
+                            options.T, options.beams, options.central_freq,
+                            options.outdir)
+            os.system('ansible "arts0%0.2d.apertif" -a "nice python %s %s/CB%0.2d --cb %d --t %f --T %f --beams %s --central_freq %f --outdir %s"' % ansible_args)
+            # copy the data to the collectdir
+            if options.collectdir:
+                if (socket.gethostname() != 'arts041'): 
+                    print("You should probably not run this on a host different than arts041! Collect_dir may be incorrect")
+                else: 
+#                   os.system('ansible "arts0%0.2d.apertif" -a "cd %s; ionice scp CB%0.2d*.fil arts041:%s/"' % (ii+1, options.outdir, ii, options.collectdir))               
+                    os.system('mkdir -p %s ; cd %s ; scp arts0%0.2d.apertif:%s/CB%0.2d*.fil .' % (options.collectdir, options.collectdir, ii+1, options.outdir, ii))
+            os._exit(os.EX_OK) # killing this child   
+
+    for child in forked_jobs:
+        os.waitpid(child, 0)
