443dd99e024ce77cfe2ebc603aa9fc3e0d4fbea0
Liam Connor <liam.dean.connor@gmail.com>
2020-02-17 11:14:23 +0100

    writing script for polarisation calibration

--- polcal_script.py
+++ polcal_script.py
@@ -9,13 +9,14 @@ rebin_time = 1
 rebin_freq = 1
 DM = 588.1
 pulse_width = 10 # number of samples to sum over
+transpose = False
 
 dpath = '/tank/data/FRBs/FRB200216/iquv/CB05/npy/stokes*sb30.npy'
 dedisp_data_path = '/home/arts/connor/software/arts-analysis/arts-analysis/frb191108/dedispersed_FRB191108.npy'
 bandpass_path = '/home/arts/connor/software/arts-analysis/arts-analysis/frb191108/bandpass_from_3c286_alpha-0.54_CB22.npy'
 
 if generate_iquv_arr:
-	arr_list, pulse_sample = make_iquv_arr(dpath, rebin_time=rebin_time, rebin_freq=rebin_freq, dm=DM, trans=True)
+	arr_list, pulse_sample = make_iquv_arr(dpath, rebin_time=rebin_time, rebin_freq=rebin_freq, dm=DM, trans=transpose)
 	stokes_arr = np.concatenate(arr_list, axis=0).reshape(4, nfreq, -1)
 
 if not generate_iquv_arr:
