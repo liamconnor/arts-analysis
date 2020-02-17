085a0bb5ad3f8dccbe1c200d70ee1a8650f2a04b
Liam Connor <liam.dean.connor@gmail.com>
2020-02-17 11:33:53 +0100

    adding rficlean

--- pol.py
+++ pol.py
@@ -24,7 +24,7 @@ dt = 8.192e-5
 rebin_time=1
 rebin_freq=1
 
-def make_iquv_arr(dpath, rebin_time=1, rebin_freq=1, dm=0.0, trans=True):
+def make_iquv_arr(dpath, rebin_time=1, rebin_freq=1, dm=0.0, trans=True, RFI_clean=False):
 	""" Read in all 4 arrays, dedisperse, 
 	return list with median-subtracted, rebinned 
 	[arr_I, arr_Q, arr_U, arr_V]
@@ -46,6 +46,8 @@ def make_iquv_arr(dpath, rebin_time=1, rebin_freq=1, dm=0.0, trans=True):
                         arr = arr.transpose()
                 if arr.shape[0]!=1536:
                         print("Are you sure you wanted to transpose?")
+        if RFI_clean:
+	        arr = tools.cleandata(arr, clean_type='perchannel')
 		arr = tools.dedisperse(arr, dm, freq=freq)[:, :last_ind]
 		nt, nf = arr.shape[-1], arr.shape[0]
 		arr = arr - np.median(arr, axis=-1, keepdims=True)
--- polcal_script.py
+++ polcal_script.py
@@ -1,13 +1,16 @@
-from pol import *
+import numpy as np
+
+import pol
 
 generate_iquv_arr = True
 bandpass_correct = True
+RFI_clean = True
 mk_plot = True
 
 nfreq = 1536
 rebin_time = 1
 rebin_freq = 1
-DM = 588.1
+DM = 478.0
 pulse_width = 10 # number of samples to sum over
 transpose = False
 
@@ -16,7 +19,9 @@ dedisp_data_path = '/home/arts/connor/software/arts-analysis/arts-analysis/frb19
 bandpass_path = '/home/arts/connor/software/arts-analysis/arts-analysis/frb191108/bandpass_from_3c286_alpha-0.54_CB22.npy'
 
 if generate_iquv_arr:
-	arr_list, pulse_sample = make_iquv_arr(dpath, rebin_time=rebin_time, rebin_freq=rebin_freq, dm=DM, trans=transpose)
+	arr_list, pulse_sample = pol.make_iquv_arr(dpath, rebin_time=rebin_time, 
+											   rebin_freq=rebin_freq, dm=DM, trans=transpose,
+											   RFI_clean=RFI_clean)
 	stokes_arr = np.concatenate(arr_list, axis=0).reshape(4, nfreq, -1)
 
 if not generate_iquv_arr:
@@ -27,10 +32,14 @@ if not generate_iquv_arr:
 		print("No dedispersed Stokes array available. Exiting.")
 		exit()
 
+if RFI_clean:
+	for ii in range(4):
+		stokes_arr[ii] = tools.clean_type(stokes_arr[ii], clean_type='perchannel')
+
 if bandpass_correct:
 	bp_arr = np.load(bandpass_path)
 	stokes_arr /= bp_arr[None, :, None]
 
 if mk_plot:
-	plot_im_raw(stokes_arr, pulse_sample=pulse_sample)
-	plot_raw_data(stokes_arr, pulse_sample=pulse_sample)
+	pol.plot_im_raw(stokes_arr, pulse_sample=pulse_sample)
+	pol.plot_raw_data(stokes_arr, pulse_sample=pulse_sample)
