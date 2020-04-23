ee768388c684b9ed085bc071e6c1e4f1836139af
Liam Connor <liam.dean.connor@gmail.com>
2020-04-23 10:35:19 +0200

    added instructions on using with TAB

--- waterfall_sb.py
+++ waterfall_sb.py
@@ -1,53 +1,78 @@
 import optparse
 import numpy as np
-#import triggers_liam as triggers
+
 import triggers
 
 if __name__=='__main__':
     parser = optparse.OptionParser(prog="waterfall_sb.py",
-                                   version="",
-                                   usage="%prog FN_FILTERBANK_PREFIX [OPTIONS]",
-                                   description="Create diagnostic plots for individual triggers")
+          version="",
+          usage="%prog FN_FILTERBANK_PREFIX [OPTIONS]",
+          description="Create diagnostic \
+          plots for individual triggers. \
+          \
+          If you want to plot a single TAB, feed the \
+          program the full path to the .fil file. \
+          If you want to plot a single SB or all SBs, \
+          feed it just the basename without the TAB number or .fil.\
+          e.g. python waterfall_sb.py CB32_10.0sec_dm0_t03825_sb-1_tab09.fil\
+          vs.  python waterfall_sb.py CB32_10.0sec_dm0_t03825_sb-1_tab --sb all")
 
     parser.add_option('--ndm', dest='ndm', type='int',
-                      help="Number of DMs to use in DM transform (Default: 50).",
+                      help="Number of DMs for DM transform (Default: 50)",
                       default=1)
 
     parser.add_option('--mask', dest='maskfile', type='string',
-                      help="Mask file produced by rfifind. (Default: No Mask).",
+                      help="Mask file produced by rfifind. \
+                      (Default: No Mask).",
                       default=None)
 
-    parser.add_option('--save_data', dest='save_data', action='store_true', default=False)
+    parser.add_option('--save_data', dest='save_data', 
+                      action='store_true', default=False)
 
     parser.add_option('--rficlean', dest='rficlean', action='store_true',
-                      help="use rficlean if True (default False)", default=False)
+                      help="use rficlean if True (default False)", 
+                      default=False)
 
-    parser.add_option('--threshold_time', dest='threshold_time', action='store_true',
-                      help="If rficlean is True, defines threshold for time-domain clean (default 3.25)",
+    parser.add_option('--threshold_time', dest='threshold_time', 
+                      action='store_true',
+                      help="If rficlean is True, defines \
+                      threshold for time-domain clean (default 3.25)",
                       default=3.25)
 
-    parser.add_option('--threshold_frequency', dest='threshold_frequency', type=float,
-                      help="If rficlean is True, defines threshold for freqency-domain clean (default 2.5)",
+    parser.add_option('--threshold_frequency', 
+                      dest='threshold_frequency', 
+                      type=float,
+                      help="If rficlean is True, defines \
+                      threshold for freqency-domain clean (default 2.5)",
                       default=2.75)
 
     parser.add_option('--bin_size', dest='bin_size', type=int,
-                      help="If rficlean is True, defines bin size for bandpass removal (default 32)",
+                      help="Defines bin size for bandpass removal \
+                      if rficlean True",
                       default=32)
 
-    parser.add_option('--n_iter_time', dest='n_iter_time', action='store_true',
-                      help="If rficlean is True, defines number of iteration for time-domain clean (default 3)",
+    parser.add_option('--n_iter_time', dest='n_iter_time', 
+                      action='store_true',
+                      help="If rficlean is True, defines number of \
+                      iteration for time-domain clean (default 3)",
                       default=3)
 
-    parser.add_option('--n_iter_frequency', dest='n_iter_frequency', action='store_true',
-                      help="If rficlean is True, defines number of iteration for frequency-domain clean (default 3)",
+    parser.add_option('--n_iter_frequency', dest='n_iter_frequency', 
+                      action='store_true',
+                      help="If rficlean is True, defines number of \
+                      iteration for frequency-domain clean (default 3)",
                       default=3)
 
     parser.add_option('--clean_type', dest='clean_type',
-                      help="If rficlean is True, defines type of clean (default 'time')",
-                      choices=['time', 'freqency', 'both', 'perchannel'], default='time')
+                      help="If rficlean is True, defines type of \
+                      clean (default 'time')",
+                      choices=['time', 'freqency', 'both', 'perchannel'], 
+                      default='time')
 
-    parser.add_option('--subtract_zerodm', dest='subtract_zerodm', action='store_true',
-                      help="use DM=0 timestream subtraction if True (default False)", default=False)
+    parser.add_option('--subtract_zerodm', dest='subtract_zerodm', 
+                      action='store_true',
+                      help="use DM=0 timestream subtraction \
+                      if True (default False)", default=False)
 
     parser.add_option('--nfreq_plot', dest='nfreq_plot', type='int',
                       help="make plot with this number of freq channels",
@@ -82,16 +107,15 @@ if __name__=='__main__':
                       default='')
 
     parser.add_option('--tab', dest='tab', type=int,
-                      help="TAB to process (0 for IAB) (default: 0)", default=-1)
+                      help="TAB to process (0 for IAB) (default: 0)", 
+                      default=-1)
 
     parser.add_option('--sb', dest='sb', type=str, default='35',
                       help="Process synthesized beams")
 
     parser.add_option('--central_freq', dest='freq', type=int, default=1370, 
-                      help="Central frequency in zapped channels filename (Default: 1370)")
-
-    parser.add_option('--save_snippet', dest='save_snippet', action='store_true',
-                      help="save snippet of raw data to filterbank (default False)", default=False)
+                      help="Central frequency in zapped channels \
+                      filename (Default: 1370)")
 
     options, args = parser.parse_args()
     fn_fil = args[0]
@@ -140,8 +164,10 @@ if __name__=='__main__':
                  rficlean=options.rficlean, snr_comparison=-1,
                  outdir=options.outdir, sig_thresh_local=0.0,
                  subtract_zerodm=False, 
-                 threshold_time=3.25, threshold_frequency=2.75, bin_size=options.bin_size,
-                 n_iter_time=options.n_iter_time, n_iter_frequency=options.n_iter_frequency, 
+                 threshold_time=3.25, threshold_frequency=2.75, 
+                 bin_size=options.bin_size,
+                 n_iter_time=options.n_iter_time, 
+                 n_iter_frequency=options.n_iter_frequency, 
                  clean_type=options.clean_type, freq=options.freq,
                  sb_generator=sb_generator, sb=sb)
 
@@ -152,6 +178,6 @@ if __name__=='__main__':
                 sb_ = -1
             else:
                 sb_ = sb
-            fnoutdata=options.outdir+'/'+fn_fil_.split('/')[-1]+"SB%d_dedisp.npy" % sb_
+            fnoutdata=options.outdir+'/'+fn_fil_.split('/')[-1]+"SB%d_dedisp.npy"%sb_
             np.save(fnoutdata, x[1])
             print("Saved data to %s" % fnoutdata)
